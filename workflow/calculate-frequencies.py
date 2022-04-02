70fdbe3f96ed9b134bdc6887c3023993d8ddcdbd
stho32 <stho32github@gmail.com>
2022-04-02 14:47:53 +0200

    extracted the function process_srt from process_srt_files_in_folder

--- workflow/calculate-frequencies.py
+++ workflow/calculate-frequencies.py
@@ -8,18 +8,20 @@ script_directory = environment.get_path_from_filepath(__file__)
 
 files_with_subtitles = environment.get_files_that_end_with(script_directory, ".srt")
 
+def process_srt(filename):
+    text = srtreader.get_content_as_plain_text(filename)
+
+    if filename.endswith("_fr.srt"):
+        words = split_fr.split_into_words(text)
+        word_frequencies = frequencies.calculate_frequencies(words)
+        file.save_as_json(filename + ".json", word_frequencies)
+
+    file.write_text_to_file(filename + ".txt", text)
+
 def process_srt_files_in_folder():
     for filename in files_with_subtitles:
         print(f"  - processing {filename} ...")
-
-        text = srtreader.get_content_as_plain_text(filename)
-
-        if filename.endswith("_fr.srt"):
-            words = split_fr.split_into_words(text)
-            word_frequencies = frequencies.calculate_frequencies(words)
-            file.save_as_json(filename + ".json", word_frequencies)
-
-        file.write_text_to_file(filename + ".txt", text)
+        process_srt(filename)
 
 if __name__ == "__main__":
    process_srt_files_in_folder()
