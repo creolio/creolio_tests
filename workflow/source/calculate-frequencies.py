ca2bf34b876bce7573c237aae4fbde74e8dc1852
stho32 <stho32github@gmail.com>
2022-04-13 21:07:26 +0200

    seperated out the support for french in a way that it should be easy to add more languages

--- workflow/source/calculate-frequencies.py
+++ workflow/source/calculate-frequencies.py
@@ -2,19 +2,24 @@ from srtfiles import srtreader
 from helpers import environment
 from helpers import file
 from textprocessing import frequencies
-import workflow.source.split_fr as split_fr
+from languages import french
+
+supported_languages = [
+    french.French()
+]
 
 script_directory = environment.get_path_from_filepath(__file__)
 
-files_with_subtitles = environment.get_files_that_end_with(script_directory, ".srt")
+files_with_subtitles = environment.get_files_that_end_with(script_directory + "/../data", ".srt")
 
 def process_srt(filename):
     text = srtreader.get_content_as_plain_text(filename)
 
-    if filename.endswith("_fr.srt"):
-        words = split_fr.split_into_words(text)
-        word_frequencies = frequencies.calculate_frequencies(words)
-        file.save_as_json(filename + ".json", word_frequencies)
+    for language in supported_languages:
+        if filename.endswith(language.get_file_ending()):
+            words = language.split_into_words(text)
+            word_frequencies = frequencies.calculate_frequencies(words)
+            file.save_as_json(filename + ".json", word_frequencies)
 
     file.write_text_to_file(filename + ".txt", text)
 
